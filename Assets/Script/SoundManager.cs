11b840ff82643950469cd5b218434ddc635defed
Nurul Huda <nhuda55161@gmail.com>
2022-05-10 11:35:20 +0700

    Update SoundManager.cs

--- Assets/Script/SoundManager.cs
+++ Assets/Script/SoundManager.cs
@@ -7,8 +7,12 @@ public class SoundManager : MonoBehaviour
 {
     public static SoundManager instance;
     public AudioClip uiButton;
+<<<<<<< Updated upstream
 
     private AudioSource audioSfx;
+=======
+    private AudioSource audio;
+>>>>>>> Stashed changes
 
     // Start is called before the first frame update
     private void Awake() 
