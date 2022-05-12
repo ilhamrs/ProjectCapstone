2bf0e52a259e86e682bd2e4848b00191b90b10ba
lipaw08 <100027789+lipaw08@users.noreply.github.com>
2022-05-12 19:15:30 +0700

    Menyelesaikan Bagian Main Menu + Nambahin aseet-asset baru
    Bagian Pause Menu pada gameplay sudah diperbaiki, seperti menambahkan pause dan play
    Ingin merencanakan menambahkan ganti bahasa pada bagian Main Menu(Belum di implementasikan)
    
    *bagian bahasa blm coba diimplementasikan karena ada urusan mendadak

---
+++ Assets/Script/Language.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Language : MonoBehaviour
+{
+
+    [Header("Toggle")]
+    public Toggle IndonesiaToggle;
+    public Toggle EnglishToggle;
+
+    public void ResetLanguage()
+    {
+        PlayerPrefs.SetInt("Indonesia", 0);
+        PlayerPrefs.SetInt("English", 0);
+    }
+
+    public void SetIndonesia()
+    {
+        ResetLanguage();
+        PlayerPrefs.SetInt("Indonesia", 1);
+    }
+
+    public void SetEnglish()
+    {
+        ResetLanguage();
+        PlayerPrefs.SetInt("English", 1);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PlayerPrefs.HasKey("Indonesia") && PlayerPrefs.GetInt("Indonesia") == 1)
+        {
+            if (IndonesiaToggle) IndonesiaToggle.isOn = true;
+        }
+        if (PlayerPrefs.HasKey("English") && PlayerPrefs.GetInt("English") == 1)
+        {
+            if (EnglishToggle) EnglishToggle.isOn = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/LanguageTarget.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LanguageTarget : MonoBehaviour
+{
+
+    Text TargetText;
+
+    [Header("Language Settings")]
+    [TextArea(5, 10)]
+    public string IndonesianText;
+    [TextArea(5, 10)]
+    public string EnglishText;
+
+
+
+    void ChangeLanguage()
+    {
+        if (PlayerPrefs.GetInt("Indonesia") == 1)
+        {
+            TargetText.text = IndonesianText;
+        }
+        if (PlayerPrefs.GetInt("English") == 1)
+        {
+            TargetText.text = EnglishText;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TargetText = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void LateUpdate()
+    {
+        ChangeLanguage();
+    }
+}
