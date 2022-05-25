3b0b61d9bd0963d6edbe4b008b72cd7540b87514
Ilham Ramadhani Saputro <dkijakartaramasaputro@gmail.com>
2022-05-25 18:08:15 +0700

    Menambah mekanisme timer/waktu

---
+++ Assets/Script/Timer.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] Text timerText;
+    float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        
+        float minutes = Mathf.FloorToInt(timer / 60);
+        float seconds = Mathf.FloorToInt(timer % 60);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
