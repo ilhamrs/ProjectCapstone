7a013e6cf97a1412d9a11ba114da8836cc9f1af7
Ilham Ramadhani Saputro <dkijakartaramasaputro@gmail.com>
2022-05-25 18:47:18 +0700

    Menambah mekanisme revisi
    revisi bertambah saat pemain terkena trap

--- Assets/Script/FallingObjects.cs
+++ Assets/Script/FallingObjects.cs
@@ -4,14 +4,28 @@ using UnityEngine;
 
 public class FallingObjects : MonoBehaviour
 {
+    [SerializeField] Revisi revisi;
+    public GameObject trapObject;
     public Rigidbody2D rb;
     public BoxCollider2D boxCollider2D;
     public bool isFalling = false;
+
+    Vector2 originalPos;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         boxCollider2D = GetComponent<BoxCollider2D>();
+    }
+
+    private void Awake()
+    {
+        originalPos = transform.localPosition;
+    }
+
+    private void OnEnable()
+    {
+        transform.localPosition = originalPos;
     }
 
     private void Update()
@@ -22,13 +36,16 @@ public class FallingObjects : MonoBehaviour
     {
         rb.gravityScale = 5;
         isFalling = true;
+        trapObject.SetActive(true);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            Destroy(gameObject);
+            //Destroy(gameObject);
+            revisi.getHit();
+            StartCoroutine(Reset(1));
         }
         else
         {
@@ -36,5 +53,21 @@ public class FallingObjects : MonoBehaviour
             boxCollider2D.enabled = false;
             //disini bisa dikasih delay biar dia ilang gitu
         }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            revisi.getHit();
+        }
+    }
+
+    IEnumerator Reset(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        trapObject.SetActive(false);
+        Debug.Log("reset");
     }
 }
---
+++ Assets/Script/Revisi.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Revisi : MonoBehaviour
+{
+    [SerializeField] Text revisiText;
+    int jmlRevisi;
+    // Start is called before the first frame update
+    void Start()
+    {
+        jmlRevisi = 0;
+        revisiText.text = jmlRevisi.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void getHit()
+    {
+        jmlRevisi++;
+        Debug.Log("get hit");
+        revisiText.text = jmlRevisi.ToString();
+    }
+}
