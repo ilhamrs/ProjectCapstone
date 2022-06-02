c9da6b21fdbb50d8eadac51fb7585f7b3a9e9529
lipaw08 <100027789+lipaw08@users.noreply.github.com>
2022-06-02 15:27:43 +0700

    Tambahin Teleporter

---
+++ Assets/Script/SplashScreenEffect/FadeEffect.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeEffect : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void delayFade() 
+    {
+        StartCoroutine(Delay());
+    }
+    IEnumerator Delay() 
+    {
+        yield return new WaitForSeconds(2);
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Script/Teleport/PlayerTeleport.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerTeleport : MonoBehaviour
+{
+    private GameObject currentTeleporter;
+    public UnityEvent TriggerEvent;
+
+    public void InvokeTrigger()
+    {
+        TriggerEvent.Invoke();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            if (currentTeleporter != null)
+            {
+                transform.position = currentTeleporter.GetComponent<Teleporter>().GetDestination().position;
+                TriggerEvent.Invoke();
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Teleporter"))
+        {
+            currentTeleporter = collision.gameObject;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Teleporter"))
+        {
+            if (collision.gameObject == currentTeleporter)
+            {
+                currentTeleporter = null;
+            }
+        }
+    }
+}
