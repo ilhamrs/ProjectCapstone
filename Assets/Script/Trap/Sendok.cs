5020f561d4c636a671d959fe9d094d335887660b
lipaw08 <100027789+lipaw08@users.noreply.github.com>
2022-06-09 15:36:48 +0700

    Nambahin Asset + Desain Level lantai 2
    Desain Level Lantai 2:
    + Animasi Pintu
    + Trap Sendok
    - Trap Lampu

---
+++ Assets/Script/Trap/Sendok.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sendok : MonoBehaviour
+{
+    [SerializeField] Revisi revisi;
+    public GameObject trapObject;
+    public GameObject triggerTrap;
+    public BoxCollider2D boxCollider2D;
+    public bool isOpen = false;
+
+    Vector2 originalPos;
+    private void Start()
+    {
+        boxCollider2D = GetComponent<BoxCollider2D>();
+    }
+
+    private void Awake()
+    {
+        originalPos = transform.localPosition;
+    }
+
+    private void OnEnable()
+    {
+        transform.localPosition = originalPos;
+    }
+    public void Muncul()
+    {
+        isOpen = true;
+        boxCollider2D.enabled = true;
+        //nyalain object matiin trigger
+        ResetObject reset = gameObject.GetComponent<ResetObject>();
+        reset.active();
+        StartCoroutine(Sendokmuncul());
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            revisi.getHit();
+            StartCoroutine(Reset(1));
+            //mungkin disini pengen ditambahin trigger event buat setactive checkpoint trap
+        }
+    }
+    IEnumerator Reset(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        //nyalain trigger matiin object
+        ResetObject reset = gameObject.GetComponent<ResetObject>();
+        reset.nonactive();
+        Debug.Log("reset");
+    }
+
+    //ini buat restart yang ditaro di checkpoint
+    public void restart()
+    {
+        StartCoroutine(Reset(1));
+    }
+
+    IEnumerator Sendokmuncul() 
+    {
+        yield return new WaitForSeconds(0.5f);
+        boxCollider2D.enabled = false;
+    }
+}
