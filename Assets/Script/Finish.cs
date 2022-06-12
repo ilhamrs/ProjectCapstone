32397e1ff7c86a565aa410f7dea3ea12a2dd5597
Ilham Ramadhani Saputro <dkijakartaramasaputro@gmail.com>
2022-06-12 12:03:18 +0700

    menambah mekanisme nilai dan high score
    pada menu finish

--- Assets/Script/Finish.cs
+++ Assets/Script/Finish.cs
@@ -12,11 +12,21 @@ public class Finish : MonoBehaviour
 
     [SerializeField] Text timerText;
     [SerializeField] Text revisiText;
+    [SerializeField] GameObject gradeImage;
+    [SerializeField] GameObject highScoreText;
+    ChapterData dataChapOne;
+
+    [Header("Grade Image")]
+    [SerializeField] Sprite AGrade;
+    [SerializeField] Sprite BGrade;
+    [SerializeField] Sprite CGrade;
+    [SerializeField] Sprite EGrade;
     // Start is called before the first frame update
     void Start()
     {
         finishMenu.SetActive(false);
         Time.timeScale = 1;
+        dataChapOne = ChapterOneSaveSystem.LoadGame();
     }
 
     // Update is called once per frame
@@ -39,6 +49,41 @@ public class Finish : MonoBehaviour
 
             timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
             revisiText.text = revisi.getRevisi().ToString();
+
+            CheckHighScore();
+            CheckGrade();
+        }
+    }
+
+    void CheckGrade()
+    {
+        if (revisi.getRevisi() < 10)
+        {
+            gradeImage.GetComponent<Image>().sprite = AGrade;
+        }
+        else if (revisi.getRevisi() < 20)
+        {
+            gradeImage.GetComponent<Image>().sprite = BGrade;
+        }
+        else if (revisi.getRevisi() < 30)
+        {
+            gradeImage.GetComponent<Image>().sprite = CGrade;
+        }
+        else
+        {
+            gradeImage.GetComponent<Image>().sprite = EGrade;
+        }
+    }
+
+    void CheckHighScore()
+    {
+        if (revisi.getRevisi() <= dataChapOne.jmlRevisi && timer.getTimer() <= dataChapOne.time)
+        {
+            highScoreText.SetActive(true);
+        }
+        else
+        {
+            highScoreText.SetActive(false);
         }
     }
 }
