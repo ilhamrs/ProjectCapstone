e24ea3d00e399daa0ee2b4b233472ac6c758974e
lipaw08 <100027789+lipaw08@users.noreply.github.com>
2022-06-15 23:18:41 +0700

    trying fade effect di panel cuma broken

--- Assets/Script/RevisiPanel.cs
+++ Assets/Script/RevisiPanel.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.Events;
 
 public class RevisiPanel : MonoBehaviour
 {
     [SerializeField] GameObject revisiPanel;
     [SerializeField] Revisi revisi;
     [SerializeField] TextMeshProUGUI revisiText;
+
+    public UnityEvent TriggerEvent;
 
     //[SerializeField] GameObject dudung;
     // Start is called before the first frame update
@@ -24,7 +27,8 @@ public class RevisiPanel : MonoBehaviour
 
     public IEnumerator showPanel()
     {
-        yield return new WaitForSeconds(0.7f);
+        yield return new WaitForSeconds(0.8f);
+        TriggerEvent.Invoke();
         revisiPanel.SetActive(true);
         revisiText.text = revisi.getRevisi().ToString();
         StartCoroutine(hidePanel());
