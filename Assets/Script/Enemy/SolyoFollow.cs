7a65e5bde6845387c6ba9cbd59f5194e1ed73c70
lipaw08 <100027789+lipaw08@users.noreply.github.com>
2022-11-22 15:25:01 +0700

    Solyo Aman

---
+++ Assets/Script/Enemy/SolyoFollow.cs
@@ -0,0 +1,129 @@
+using System;
+using System.Data;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class SolyoFollow : MonoBehaviour
+{
+    [SerializeField]
+    public Transform player;
+    public Transform Kerumunan;
+    [SerializeField] Revisi revisi;
+    public GameObject trapObject;
+    public GameObject triggerTrap;
+    public BoxCollider2D boxCollider2D;
+
+    public AudioSource sfxDog;
+    public Animator anim;
+
+    [SerializeField]
+    float Range;
+    [SerializeField]
+    float speed;
+
+    Vector2 originalPos;
+
+    bool getBone;
+
+    Rigidbody2D rb;
+
+    [Header("Trigger Reset")]
+    [SerializeField] Transform reset;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        boxCollider2D = GetComponent<BoxCollider2D>();
+
+        sfxDog.Play();
+    }
+
+    private void Awake()
+    {
+        originalPos = transform.localPosition;
+        getBone = false;
+    }
+
+    private void OnEnable()
+    {
+        transform.localPosition = originalPos;
+        getBone = false;
+    }
+
+    void Update()
+    {
+        float distance = Vector2.Distance(transform.position, player.position);
+        float distance1 = Vector2.Distance(transform.position, Kerumunan.position);
+
+        if (distance < Range && !getBone)
+        {
+            anim.SetTrigger("Attack");
+            FollowPlayer();
+        }
+        else if (distance1 < Range)
+        {
+            //FollowTulang();
+            //distance - 5;
+            //anim.SetTrigger("Take");
+        }
+    }
+
+    public void FollowPlayer()
+    {
+        ResetObject reset = gameObject.GetComponent<ResetObject>();
+        reset.active();
+        if (transform.position.x < player.position.x)
+        {
+            rb.velocity = new Vector2(speed, 0);
+            transform.localScale = new Vector2(1, 1);
+        }
+        else
+        {
+            rb.velocity = new Vector2(-speed, 0);
+            transform.localScale = new Vector2(-1, 1);
+        }
+    }
+
+    public void FollowTulang()
+    {
+        if (transform.position.x < Kerumunan.position.x)
+        {
+            rb.velocity = new Vector2(speed, 0);
+            transform.localScale = new Vector2(1, 1);
+        }
+        else
+        {
+            rb.velocity = new Vector2(-speed, 0);
+            transform.localScale = new Vector2(-1, 1);
+
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            reset.GetComponent<Reset>().ActivateRoom(true);
+            revisi.getHit();
+            boxCollider2D.enabled = false;
+            StartCoroutine(Reset(1));
+        }
+
+        if (collision.tag == "Skeleton")
+        {
+            anim.SetTrigger("Idle");
+            getBone = true;
+            boxCollider2D.enabled = false;
+        }
+    }
+
+    IEnumerator Reset(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        ResetObject reset = gameObject.GetComponent<ResetObject>();
+        reset.nonactive();
+        boxCollider2D.enabled = true;
+        Debug.Log("reset");
+    }
+}
