9ee37e0f33f0fbac512e3e9540a99dca2e72fb40
lipaw08 <100027789+lipaw08@users.noreply.github.com>
2022-11-23 10:52:36 +0700

    Stage 3 Obstacle sudah benar

--- Assets/Script/Trap/InstantDeathSpoon.cs
+++ Assets/Script/Trap/InstantDeathSpoon.cs
@@ -6,22 +6,51 @@ using UnityEngine.Events;
 public class InstantDeathSpoon : MonoBehaviour
 {
     [SerializeField] Revisi revision;
+
+    public GameObject trapObject;
+    public GameObject triggerTrap;
+    Vector2 originalPos;
+
+    [Header("Trigger Reset")]
+    [SerializeField] Transform reset;
+
+    private void Awake()
+    {
+        originalPos = transform.localPosition;
+    }
+
+    private void OnEnable()
+    {
+        transform.localPosition = originalPos;
+    }
     protected void OnCollisionEnter2D(Collision2D other)
     {
         var player = other.collider.GetComponent<PlayerMove>();
         if (player != null)
         {
+            reset.GetComponent<Reset>().ActivateRoom(true);
             player.Die();
             revision.getHit();
+            StartCoroutine(Reset(1));
         }
     }
 
     protected void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Player") 
+        if (collision.tag == "Player")
         {
+            reset.GetComponent<Reset>().ActivateRoom(true);
             collision.GetComponent<PlayerMove>().Die();
             revision.getHit();
+             StartCoroutine(Reset(1));
         }
+    }
+     IEnumerator Reset(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        ResetObject reset = gameObject.GetComponent<ResetObject>();
+        reset.nonactive();
+        Debug.Log("reset");
     }
 }
